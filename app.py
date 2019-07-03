be46c8ec16be9c3e6e6090721dbb7ac1e3c1fe5d
nitro9a <nitro9a@gmail.com>
2019-07-02 21:48:07 -0400

    created layout.kv bound button and label to new layout
    added scalelabel.py
    changed button and label to ScaleButton and ScaleLabel

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import sqlite3
 import random
-from utils import database
+from utils import database, scalelabel
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
 word_dict = {}
 
 white = (1,1,1,1)
@@ -14,17 +19,31 @@ yellow = (.9, .8, 0, 1)
 red = (.9, .3, 0, 1)
 orange = (.9, .5, 0, 1)
 
-class appLayout(RelativeLayout):
+
+
+class UnreadWords(Screen):
+    pass
+
+class ReadWords(Screen):
+    pass
+
+class Favorites(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class WordADay(Screen):
+
+    get = ObjectProperty(None)
+    translation = ObjectProperty(None)
 
     def __init__(self, **kwargs):
-        super(appLayout, self).__init__(**kwargs)
-        self.button= Button(text= 'Click Here', font_size = 30, color = yellow, pos_hint = {'center_x': 0.5, 'center_y': .8}, size_hint = (.25, .25))
+        super(WordADay, self).__init__(**kwargs)
         self.label = Label(text = '')
-        self.button.bind(on_press = self.random_word)
-        self.add_widget(self.button)
         self.add_widget(self.label)
 
-    def random_word(self, event):
+    def random_word(self):
 
         #get a word from the unread table
         get_words(database.access_word_table('mando-a_unread.db'))
@@ -38,8 +57,8 @@ class appLayout(RelativeLayout):
         pro = (str(random_w['Pronunciation']))
         eng =  (str(random_w['English']))
         print (str(random_w['Word']))
-        self.label.text = (f'Word: {str(word)}\nPronunciation: {str(pro)}\nEnglish: {str(eng)}')
-
+        self.translation.text = (f'Word: {str(word)}\nPronunciation: {str(pro)}\nEnglish: {str(eng)}')
+        #TODO look at exercise dice, gen_ex_die, for line in text - figure out text wrapping
         #remove fetched word from the unread table
         database.remove_word('mando-a_unread.db', str(random_w['Word']))
 
@@ -64,20 +83,19 @@ def get_words(database):
 
         word_dict.update(entry)
 
+kv = Builder.load_file("layout.kv")
 
 class WordApp(App):
-
     def build(self):
-        appL = appLayout()
-        return appL
+        return kv
 
 if __name__=="__main__":
     WordApp().run()
 
 
 
-
-
+    #TODO Find out what happens when all words are removed
+    #TODO Make pages
     #TODO Create a new list of unread words, 'remove word' from 'unread word' list - FINISHED
     #TODO Add random word to a 'read words' list - FINISHED
     #TODO Add reset
