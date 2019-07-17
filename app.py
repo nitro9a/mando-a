f8570fb64aa084f2358d0213b40ffaa2bd2de8be
nitro9a <nitro9a@gmail.com>
2019-07-17 15:06:43 -0400

    - popup window now displays related information correctly
    - added all from recycleselect.py to app.py and layout.kv

--- app.py
+++ app.py
@@ -2,24 +2,75 @@ import sqlite3
 import csv
 import random
 import textwrap
-from utils import database, scalelabel, scrollablelabel, recycleselect
+from utils import database, scalelabel, scrollablelabel
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.label import Label
-from kivy.lang import Builder
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.properties import ObjectProperty, ListProperty, StringProperty
-
-word_dict = {}
+from kivy.properties import ObjectProperty, ListProperty, StringProperty, BooleanProperty
 
 white = (1,1,1,1)
 black = (0,0,0,0)
 yellow = (.9, .8, 0, 1)
 red = (.9, .3, 0, 1)
 orange = (.9, .5, 0, 1)
+
+class MessageBox(Popup):
+    def popup_dismiss(self):
+        self.dismiss()
+
+    obj = ObjectProperty(None)
+    obj_text = StringProperty('')
+
+    def __init__(self, obj, **kwargs):
+        super(MessageBox, self).__init__(**kwargs)
+        self.obj = obj
+
+        # set the Popup text to the pronunciation and translation
+        # from the unread_dict
+        word_data = kv.get_screen('unread').unread_dict[obj.text]
+        self.obj_text = word_data[0] + '\n' + word_data[1] + '\n' + word_data[2]
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
+    """ Adds selection and focus behaviour to the view. """
+
+class SelectableButton(RecycleDataViewBehavior, Button):
+    """ Add selection support to the Label """
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        """ Catch and handle the view changes """
+        self.index = index
+        #print(type(data))
+        #print(f'Data: {data.items()}, Index: {index},rv: {rv}, Type: {type(data)}')
+        return super(SelectableButton, self).refresh_view_attrs(rv, index, data)
+
+    def apply_selection(self, rv, index, is_selected):
+        self.selected = is_selected
+
+    def on_press(self):
+        popup = MessageBox(self)
+        popup.open()
+
+    def update_changes(self, txt):
+        self.text = txt
+
+class RV(RecycleView):
+    #data_items = ListProperty([])
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+
 
 class ReadWords(Screen):
     pass
@@ -31,11 +82,15 @@ class WindowManager(ScreenManager):
     pass
 
 class WordADay(Screen):
+
+    word_dict = {}
+
     get = ObjectProperty(None)
     translation = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(WordADay, self).__init__(**kwargs)
+        self.unread_dict = {}
 
     def random_word(self):
         con = sqlite3.connect('mando-a_unread.db')
@@ -58,7 +113,7 @@ class WordADay(Screen):
                 }
                 }
 
-                word_dict.update(entry)
+                self.word_dict.update(entry)
 
         def get_random_word():
             #get words from the unread table and add them to word_dict using function "get_words"
@@ -68,7 +123,7 @@ class WordADay(Screen):
 
             try:
                 #get a row from word_dict
-                random_w = random.choice(word_dict)
+                random_w = random.choice(self.word_dict)
 
                 #random_w is the entire dictionary entry - I need to access just the 'Word' portion
                 word = (str(random_w['Word']))
@@ -89,7 +144,7 @@ class WordADay(Screen):
                 #add fetched word to the read table
                 database.add_word('mando-a_read.db', random_w['Word'], random_w['Pronunciation'], random_w['English'], Read=1)
 
-                word_dict.clear()
+                self.word_dict.clear()
 
             except KeyError:
                 print("key error")
@@ -101,12 +156,15 @@ class WordADay(Screen):
 
 class UnreadWords(Screen):
 
+    unread_dict = {}
+
     unread_table = ObjectProperty(None)
     reset = ObjectProperty(None)
     rows = ListProperty([("Mandoa", "Pronunciation", "English")])
 
     def __init__(self, **kwargs):
         super(UnreadWords, self).__init__(**kwargs)
+        self.unread_dict = {}
 
     def reset_databases(self):
 
@@ -160,6 +218,11 @@ class UnreadWords(Screen):
         cursor.execute("SELECT Mandoa, Pronunciation, English from Mando_a")
         self.rows = cursor.fetchall()
 
+        for row in self.rows:
+            self.unread_dict[row[0]] = [row[0], row[1], row[2]]
+
+        self.ids.dat.data = [{'text': key} for key in self.unread_dict.keys()]
+        print(self.unread_dict)
 kv = Builder.load_file("layout.kv")
 
 class WordApp(App):
